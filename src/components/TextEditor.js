c6d117b2fd53e964ba28ae271f76503c9ba2f9a4
NEV117 <116839735+NEV117@users.noreply.github.com>
2023-01-22 22:02:46 -0500

    first commit

---
+++ src/components/TextEditor.js
@@ -0,0 +1,32 @@
+import React, { useCallback, useEffect, useState } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+];
+
+export default function TextEditor() {
+  const [quill, setQuill] = useState();
+
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapperRef.innerHtml = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    setQuill(q);
+  }, []);
+  return <div className="container" ref={wrapperRef}></div>;
+}
